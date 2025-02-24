ef44f3e1c9c398b9696eb6f020c50cf4b15d1421
Emil Ivanov <emil.ivanov@clever-soft.com>
2025-02-24 14:04:55 +0200

    sales person table

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  //create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Emil", "Ivanov", 'emil.ivanov@test.com', 50000),
+    new SalesPerson("John", "Doe", 'john.doe@test.com', 40000),
+    new SalesPerson("Claire", "Murphy", 'claire.murphy@test.com', 30000),
+    new SalesPerson("Mai", "Truong", 'mai.truong@test.com', 30000),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
