f7359d77421ef5fa9b9ec33cac5800a551d8d0f3
Emil Ivanov <emil.ivanov@clever-soft.com>
2025-03-02 16:10:05 +0200

    introduced angular component for products list

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/products-list/products-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/products-list/products-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
+  // templateUrl: './products-list.component.html',
+  templateUrl: './products-list-table.component.html',
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
   }
 
 }
